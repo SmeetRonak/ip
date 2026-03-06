59a7f7320be9bbc90be0c5d0d2b35c2beaec097d
unknown <smileesmeet@gmail.com>
2026-03-06 21:44:11 +0800

    refactor: add javadoc comments

--- src/main/java/chatbot/Friday.java
+++ src/main/java/chatbot/Friday.java
@@ -3,6 +3,7 @@ package chatbot;
 import ui.FridayUI;
 
 //follows code modularity
+//added javaDoc comments
 public class Friday {
     public static void main(String[] args) {
         new FridayUI().start();
